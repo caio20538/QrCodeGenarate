5b3b2565aa96d9f26e3564e128434e867eb3cddc
caio20538 <caio20538@gmail.com>
2025-05-27 13:04:03 -0300

    Gerador de Qr code concluido
    Signed-off-by: caio20538 <caio20538@gmail.com>

--- QrCodeGeneretor/src/main/java/com/caio/QrCodeGeneretor/controller/QrCodeController.java
+++ QrCodeGeneretor/src/main/java/com/caio/QrCodeGeneretor/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.caio.QrCodeGeneretor.controller;
 
 import com.caio.QrCodeGeneretor.dto.qrCode.QrCodeGenerateRequest;
 import com.caio.QrCodeGeneretor.dto.qrCode.QrCodeGenerateResponse;
+import com.caio.QrCodeGeneretor.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request){
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateUpload(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
+
+
     }
 
 }
